1bf4d71db4a61bfd61df00bb302ff7a52df14e5a
Jasen Pan <jasenp@ticketek.com.au>
2019-10-16 14:45:44 +1100

    dev dep

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import pkg from "./package.json";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+import uglify from "rollup-plugin-uglify-es";
 
 const external = Object.keys(pkg.peerDependencies || {});
 const outputName = "basicTest";
@@ -14,6 +15,6 @@ export default {
     { sourcemap: true, name: outputName, file: pkg.main, format: "cjs" },
     { sourcemap: true, name: outputName, file: pkg.module, format: "es" }
   ],
-  plugins: [typescript(), resolve(), commonjs()],
+  plugins: [typescript(), resolve(), commonjs(), uglify()],
   external: [...Object.keys(pkg.peerDependencies || {})]
 };
